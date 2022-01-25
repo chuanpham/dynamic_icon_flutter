51caf5e67fccae62965ee932292b0bf98cd9a268
chuanpham <pvchuan2998@gmail.com>
2022-01-25 16:00:39 +0700

    add example
    add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,60 +1,171 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:dynamic_icon_flutter/dynamic_icon_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  int batchIconNumber = 0;
+
+  String currentIconName = "?";
+
+  bool loading = false;
+
+  TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await DynamicIconFlutter.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
+    DynamicIconFlutter.getAlternateIconName().then((v) {
+      setState(() {
+        currentIconName = v ?? "`primary`";
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
     return MaterialApp(
       home: Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Dynamic App Icon'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 28),
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Current Icon Name: $currentIconName",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ),
+              OutlineButton.icon(
+                icon: const Icon(Icons.ac_unit),
+                label: const Text("Team Fortress"),
+                onPressed: () async {
+                  try {
+                    print(await DynamicIconFlutter.supportsAlternateIcons);
+                    if (await DynamicIconFlutter.supportsAlternateIcons) {
+                      await DynamicIconFlutter.setAlternateIconName(
+                          "teamfortress");
+                      _scaffoldKey.currentState?.hideCurrentSnackBar();
+                      _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                        content: Text("App icon change successful"),
+                      ));
+                      DynamicIconFlutter.getAlternateIconName().then((v) {
+                        setState(() {
+                          currentIconName = v ?? "`primary`";
+                        });
+                      });
+                      return;
+                    }
+                  } on PlatformException catch (e) {
+                    // ignore: avoid_print
+                    print(e.toString());
+                    _scaffoldKey.currentState?.hideCurrentSnackBar();
+                    _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                      content: Text("Failed to change app icon"),
+                    ));
+                  }
+                },
+              ),
+              OutlineButton.icon(
+                icon: const Icon(Icons.ac_unit),
+                label: const Text("Photos"),
+                onPressed: () async {
+                  try {
+                    if (await DynamicIconFlutter.supportsAlternateIcons) {
+                      await DynamicIconFlutter.setAlternateIconName("photos");
+                      _scaffoldKey.currentState?.hideCurrentSnackBar();
+                      _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                        content: Text("App icon change successful"),
+                      ));
+                      DynamicIconFlutter.getAlternateIconName().then((v) {
+                        setState(() {
+                          currentIconName = v ?? "`primary`";
+                        });
+                      });
+                      return;
+                    }
+                  } on PlatformException {
+                  } catch (e) {}
+                  _scaffoldKey.currentState?.hideCurrentSnackBar();
+                  _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                    content: Text("Failed to change app icon"),
+                  ));
+                },
+              ),
+              OutlineButton.icon(
+                icon: const Icon(Icons.ac_unit),
+                label: const Text("Chills"),
+                onPressed: () async {
+                  try {
+                    if (await DynamicIconFlutter.supportsAlternateIcons) {
+                      await DynamicIconFlutter.setAlternateIconName("chills");
+                      _scaffoldKey.currentState?.hideCurrentSnackBar();
+                      _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                        content: Text("App icon change successful"),
+                      ));
+                      DynamicIconFlutter.getAlternateIconName().then((v) {
+                        setState(() {
+                          currentIconName = v ?? "`primary`";
+                        });
+                      });
+                      return;
+                    }
+                  } on PlatformException {
+                  } catch (e) {}
+                  _scaffoldKey.currentState?.hideCurrentSnackBar();
+                  _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                    content: Text("Failed to change app icon"),
+                  ));
+                },
+              ),
+              const SizedBox(
+                height: 28,
+              ),
+              OutlineButton.icon(
+                icon: const Icon(Icons.restore_outlined),
+                label: const Text("Restore Icon"),
+                onPressed: () async {
+                  try {
+                    if (await DynamicIconFlutter.supportsAlternateIcons) {
+                      await DynamicIconFlutter.setAlternateIconName(null);
+                      _scaffoldKey.currentState?.hideCurrentSnackBar();
+                      _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                        content: Text("App icon restore successful"),
+                      ));
+                      DynamicIconFlutter.getAlternateIconName().then((v) {
+                        setState(() {
+                          currentIconName = v ?? "`primary`";
+                        });
+                      });
+                      return;
+                    }
+                  } on PlatformException {
+                  } catch (e) {}
+                  _scaffoldKey.currentState?.hideCurrentSnackBar();
+                  _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                    content: Text("Failed to change app icon"),
+                  ));
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
